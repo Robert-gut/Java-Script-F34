e007bd9a63e588cdc527ed1e5e102f98223e08ac
Robert <Robert@step.rivne>
2024-01-22 20:46:34 +0200

    js intro

---
+++ 1 js intro/script/main.js
@@ -0,0 +1,125 @@
+// ! типи змінних викорис для зберігання даних
+
+//* var , let , const 
+// тип змінної => name змінної = дані
+
+
+// var email = 'test@gmail.com'
+// console.log(email)
+// email = 'test2@gmail.com'
+// console.log(email);
+
+// let number = 10
+// const pi = 3.14
+
+// console.log('namber ', number);
+// console.log('pi ',pi);
+
+// number = 22
+// pi = 1.14
+
+// console.log('namber ', number);
+// console.log('pi ',pi);
+
+//! типи даних
+
+//? 1 number
+
+// let age = 20
+
+// console.log('Age = ', age)
+// console.log('Age = ', typeof age)
+
+// const value = 3.14
+
+// console.log('PI ', value)
+// console.log('PI ', typeof value)
+
+//? 2 string
+// ''
+// const name = 'Bill'
+
+// console.log('Name => ', name)
+// console.log('Name => ', typeof name)
+
+// ""
+// let email = "test@gmail.com"
+
+// console.log('email => ', email)
+// console.log('email => ', typeof email)
+
+// ``
+
+// const userName = `Ben`
+
+// console.log('userName => ', userName)
+// console.log('userName => ', typeof userName)
+
+//? 3 boolean
+// true || folse
+
+// let isActivate = false
+
+// console.log("isActivate ", isActivate)
+// console.log("isActivate ", typeof isActivate)
+
+// isActivate = true
+
+// console.log("isActivate ", isActivate)
+// console.log("isActivate ", typeof isActivate)
+
+// ? 4 undefaint
+
+// let any
+
+// console.log('Any => ', any);
+// console.log('Any => ', typeof any);
+
+// any = 'test'
+// console.log('Any => ', typeof any);
+
+// any = 20
+// console.log('Any => ', typeof any);
+
+// ? 5 null
+
+// let port = null
+
+// console.log('PORT => ', port);
+// console.log('PORT => ', typeof port);
+
+// port = 5500
+
+
+// =======================================================================================================
+
+// ! матиматичні дії
+
+const a = 5.2, b = 10, c = 2
+// 10/5.2= 1 => 4.8
+let rusult = 0
+
+rusult = a + b//додавання
+rusult = a - b//віднімання
+rusult = a * b//множення
+rusult = a / b//ділення
+rusult = b % a//остача від ділення
+
+rusult = (2 + 2) * 2
+
+// console.log(rusult);
+
+// =================================
+
+const hello = 'hello it`s my first app'
+// alert(hello)
+
+// const num1 = +prompt('Enter number 1!')
+// const num2 = +prompt('Enter number 2!')
+// alert(num1 + num2)
+
+// const day = prompt('Enter day!')
+
+// document.write(day)
+
+
